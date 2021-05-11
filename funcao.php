31ffe4403f309aada70a40635f63dcd66ccb3b7e
Wesley Antonio Lehmkuhl <lehmkuhl.wesley@gmai.com>
2021-05-11 00:16:57 -0300

    Arquivos Iniciais do Projeto: PHP +bootstrap

---
+++ funcao.php
@@ -0,0 +1,3 @@
+funcao.php
+
+<button type="button" class="btn btn-primary">Primary</button>
---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+
+
+  <head>
+   
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+
+    <title>L.DADO....S</title>
+      
+
+</head>
+  
+<body>
+
+  <div>
+
+
+    <input type="button" value="Jogador1" onclick="window.alert(random_element(v));">
+  
+
+
+  </div>
+
+
+  
+    
+ </body>
+
+
+</html>
+      
+
+
+
+    
+
+    
+
+    
+   
+
+ 
+    
+
+
+  
+  
